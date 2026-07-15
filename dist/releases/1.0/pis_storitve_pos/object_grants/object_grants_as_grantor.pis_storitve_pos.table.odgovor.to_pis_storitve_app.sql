137ee39292753175d52d1eb39ae615d2ebe67abd
Dominik Lebar <dominik.lebar@src.si>
2026-07-15 14:10:47 +0200

    Release 1.0

---
+++ dist/releases/1.0/pis_storitve_pos/object_grants/object_grants_as_grantor.pis_storitve_pos.table.odgovor.to_pis_storitve_app.sql
@@ -0,0 +1,12 @@
+grant delete on pis_storitve_pos.odgovor to pis_storitve_app;
+
+grant insert on pis_storitve_pos.odgovor to pis_storitve_app;
+
+grant select on pis_storitve_pos.odgovor to pis_storitve_app;
+
+grant update on pis_storitve_pos.odgovor to pis_storitve_app;
+
+
+
+
+-- sqlcl_snapshot {"hash":"8a6748f938b24e79b2713a26ad23d1d1fd10ea09","type":"OBJECT_GRANT","name":"object_grants_as_grantor.PIS_STORITVE_POS.TABLE.ODGOVOR.to_PIS_STORITVE_APP","schemaName":"PIS_STORITVE_POS","sxml":""}
